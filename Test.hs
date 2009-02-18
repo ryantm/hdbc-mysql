b7e75795122a1d334642e892121c8152bdae3344
waterson <waterson@maubi.net>
2009-02-18 21:27:07 +0000

    more thrashy test
    --HG--
    extra : convert_revision : 68a6e0e3c48744cd63b2ea356cc2dd01d9f5718d

--- Test.hs
+++ Test.hs
@@ -1,28 +1,17 @@
 module Main where
 
+import System.IO.Unsafe
 import Control.Monad
 import Database.HDBC
 import Database.HDBC.MySQL
 
 go :: IO ()
 go = do conn <- connectMySQL defaultMySQLConnectInfo
---                { mysqlPort = 13306
---                , mysqlDatabase = "event"
---                }
+                { mysqlHost = "madchenwagen"
+                }
 
-        putStrLn $ "you are connected to " ++ (hdbcDriverName conn) ++
-                     " server version " ++ (dbServerVer conn) ++
-                     " via client version " ++ (hdbcClientVer conn)
-
-        tables <- getTables conn
-        putStrLn $ "the tables in this database are " ++ (show tables)
-
-        stmt <- prepare conn "SELECT NOW()"
-        execute stmt []
-        rows <- fetchAllRows stmt
+        rows <- quickQuery' conn "SELECT * FROM album" []
         forM_ rows $ \row -> putStrLn $ show row
-        disconnect conn
-
 
 main :: IO ()
-main = handleSqlError go
+main = handleSqlError (replicateM_ 1000 go)
