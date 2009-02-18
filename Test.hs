c3e7869dbe3376897a17db56b2a8a6dd1e223fe2
waterson <waterson@maubi.net>
2009-02-18 22:15:43 +0000

    more testing

--- Test.hs
+++ Test.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import System.IO.Unsafe
 import Control.Monad
 import Database.HDBC
 import Database.HDBC.MySQL
@@ -10,8 +9,20 @@ go = do conn <- connectMySQL defaultMySQLConnectInfo
                 { mysqlHost = "madchenwagen"
                 }
 
-        rows <- quickQuery' conn "SELECT * FROM album" []
-        forM_ rows $ \row -> putStrLn $ show row
+        {-
+        putStrLn $ "driver " ++ (show $ hdbcDriverName conn)
+        putStrLn $ "server version " ++ (show $ dbServerVer conn)
+        tables <- getTables conn
+        forM_ tables $ \t -> do
+          putStrLn $ "table " ++ t
+          cols <- describeTable conn t
+          forM_ cols $ \(name, desc) ->
+              putStrLn $ name ++ " " ++ (show desc)
+        -}
+
+        rows0 <- quickQuery' conn "SELECT a FROM album" []
+        rows1 <- quickQuery' conn "SELECT str FROM album" []
+        forM_ (zip rows0 rows1) $ \(a, str) -> putStrLn $ "a=" ++ (show a) ++ ", str=" ++ (show str)
 
 main :: IO ()
 main = handleSqlError (replicateM_ 1000 go)
