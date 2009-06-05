b7c514dd445758c4e03f82b63937deb93908917a
waterson <waterson@maubi.net>
2009-06-05 17:57:21 +0000

    use Foreign.Concurrent to correctly incorporate Haskell finalizer for binds.  Fix handling of unsigned SqlWord32 and SqlWord64 values: tip o' the cap to yeoh@cs.wisc.edut

--- Test.hs
+++ Test.hs
@@ -6,10 +6,11 @@ import Database.HDBC.MySQL
 
 go :: IO ()
 go = do conn <- connectMySQL defaultMySQLConnectInfo
-                { mysqlHost = "madchenwagen"
+                { mysqlHost = "putterwell"
                 }
 
         {-
+
         putStrLn $ "driver " ++ (show $ hdbcDriverName conn)
         putStrLn $ "server version " ++ (show $ dbServerVer conn)
         tables <- getTables conn
@@ -18,11 +19,15 @@ go = do conn <- connectMySQL defaultMySQLConnectInfo
           cols <- describeTable conn t
           forM_ cols $ \(name, desc) ->
               putStrLn $ name ++ " " ++ (show desc)
-        -}
-
         rows0 <- quickQuery' conn "SELECT a FROM album" []
         rows1 <- quickQuery' conn "SELECT str FROM album" []
         forM_ (zip rows0 rows1) $ \(a, str) -> putStrLn $ "a=" ++ (show a) ++ ", str=" ++ (show str)
 
+         -}
+
+        stmt <- prepare conn "INSERT INTO album VALUES (?, ?)"
+        n <- execute stmt [SqlWord32 3000000000, SqlString "hello"]
+        commit conn
+
 main :: IO ()
-main = handleSqlError (replicateM_ 1000 go)
+main = handleSqlError (replicateM_ 1 go)
